e9077b6eff6bfbf57311f31e7e659750c239e066
egurtovenko <gurtovenko030984@gmail.com>
2017-09-08 12:53:28 +0300

    posts-slider done

--- src/js/main.js
+++ src/js/main.js
@@ -22,7 +22,35 @@ $(".review__slider").slick({
 
     }, {
 
-        breakpoint: 321,
+        breakpoint: 350,
+        settings: {
+            slidesToShow: 1,
+            dots: true
+        }
+    }]
+});
+$(".posts__slider").slick({
+    infinite: false,
+    dots: false,
+    responsive: [{
+
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            infinite: false
+        }
+
+    }, {
+
+        breakpoint: 780,
+        settings: {
+            slidesToShow: 2,
+            dots: false
+        }
+
+    }, {
+
+        breakpoint: 350,
         settings: {
             slidesToShow: 1,
             dots: true
